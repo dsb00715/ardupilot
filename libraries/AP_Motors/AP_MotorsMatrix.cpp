b31b6b6c71320242443b2245ce886de382eeb9e8
Deep Bhatt <dsb00715@gmail.com>
2018-09-29 13:03:11 +0200

    modification of file AP_MotorsMatrix.cpp is done

--- libraries/AP_Motors/AP_MotorsMatrix.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix.cpp
@@ -484,14 +484,24 @@ void AP_MotorsMatrix::setup_motors(motor_frame_class frame_class, motor_frame_ty
                     add_motor(AP_MOTORS_MOT_6, 120, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  3);
                     success = true;
                     break;
-                case MOTOR_FRAME_TYPE_X:
-                    add_motor(AP_MOTORS_MOT_1,  90, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  2);
-                    add_motor(AP_MOTORS_MOT_2, -90, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 5);
-                    add_motor(AP_MOTORS_MOT_3, -30, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  6);
-                    add_motor(AP_MOTORS_MOT_4, 150, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 3);
-                    add_motor(AP_MOTORS_MOT_5,  30, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 1);
-                    add_motor(AP_MOTORS_MOT_6,-150, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  4);
+				
+				case MOTOR_FRAME_TYPE_X:
+				    add_motor_raw(AP_MOTORS_MOT_1, -0.196, 0.096, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 1);
+					add_motor_raw(AP_MOTORS_MOT_2, -0.600,-0.096, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 2);
+					add_motor_raw(AP_MOTORS_MOT_3, -1.000, 0.000, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 3);
+					add_motor_raw(AP_MOTORS_MOT_4,  1.000, 0.000, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 4);
+					add_motor_raw(AP_MOTORS_MOT_5,  0.600,-0.096, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 5);
+					add_motor_raw(AP_MOTORS_MOT_6,  0.196, 0.096, AP_MOTORS_MATRIX_YAW_FACTOR_CW, 6);
+					/*  
+					Original Frame type
+					add_motor(AP_MOTORS_MOT_1,  90, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  2);
+					add_motor(AP_MOTORS_MOT_2, -90, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 5);
+					add_motor(AP_MOTORS_MOT_3, -30, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  6);
+					add_motor(AP_MOTORS_MOT_4, 150, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 3);
+					add_motor(AP_MOTORS_MOT_5,  30, AP_MOTORS_MATRIX_YAW_FACTOR_CCW, 1);
+					add_motor(AP_MOTORS_MOT_6,-150, AP_MOTORS_MATRIX_YAW_FACTOR_CW,  4);*/
                     success = true;
+					
                     break;
                 default:
                     // hexa frame class does not support this frame type
